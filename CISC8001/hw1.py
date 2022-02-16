a833a27fb0575f069040878134c31ccc6ee6f08f
lovelydett <xyt@bupt.cn>
2022-02-16 14:53:37 +0800

    [CISC8001] hw1 pyspark finished

--- CISC8001/hw1.py
+++ CISC8001/hw1.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Jun 14 10:20:19 2020
+PySpark HW1
+Author: Yuting Xie
+2022.2.16
 """
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-sc = spark.sparkContext
+from pyspark.sql.types import StructField, StructType, StringType
 
+spark = SparkSession.builder.appName('LocalTest').getOrCreate()
+sc = spark.sparkContext
 
 def logParse(log):
     log = log.replace(' -- ', ', ')
@@ -21,7 +23,6 @@ def loadRDD(filename):
 def hw1():
     # You can load the log using the following script
     rowrdd = loadRDD("data/ghtorrent-logs.txt").cache()
-    print(rowrdd.take(17))
 
     # Exercise 1: report num of ERROR and WARN
     def report_err_and_warn():
@@ -40,17 +41,111 @@ def hw1():
     #Exercise 3: for all failed request of “api_client”, report the failed count of each repository. (using groupby)
     def count_failed_repo():
         def pick(x):
-            if len(x) < 4 or x[3] != "api_client" or x[4].find("Successful request") >= 0:
-                return
+            if len(x) < 4 or x[3] != "api_client" or x[4].find("Failed request") < 0:
+                return False
+            url = x[4].replace("URL: ", ",").split(',')[1]
+            repo = url.replace("//", "/").split('/')
+            return len(repo) > 4
+        def get_repo(x):
             url = x[4]
             url = url.replace("URL: ", ",")
             url = url.split(',')
-            if len(url) < 2:
-                return
-            return url[1]
-        res = rowrdd.map(pick).collect()
-        print(res[1])
-    count_failed_repo()
+            url = url[1]
+            repo = url.replace("//", "/")
+            repo = repo.replace("?", "/")
+            repo = repo.split('/')
+            repo = repo[3] + '/' + repo[4]
+            return [repo]
+
+        res = rowrdd.filter(pick).map(get_repo) # .groupBy(lambda x : x).countByKey()
+        schema = StructType([
+            StructField("Repo", StringType(), True)
+        ])
+        df = spark.createDataFrame(res, schema=schema)
+        res = df.groupBy("Repo").count()
+        print(res.toPandas())
+    # count_failed_repo()
+
+    # Exercise 4: top-5 active repository
+    def top5_active_repo():
+        def pick(x):
+            if len(x) < 4 or x[4].find("Successful request") < 0:
+                return False
+            url = x[4]
+            url = url.replace("URL: ", ",")
+            url = url.split(',')
+            url = url[1]
+            repo = url.replace("//", "/")
+            repo = repo.split('/')
+            return len(repo) > 4
+        def get_repo(x):
+            url = x[4]
+            url = url.replace("URL: ", ",")
+            url = url.split(',')
+            url = url[1]
+            repo = url.replace("//", "/")
+            repo = repo.replace("?", "/")
+            repo = repo.split('/')
+            repo = "https://api.github.com/repos/" + repo[3] + '/' + repo[4]
+            return [repo]
+
+        res = rowrdd.filter(pick).map(get_repo) # .groupBy(lambda x : x).countByKey()
+        schema = StructType([
+            StructField("Repo", StringType(), True)
+        ])
+        df = spark.createDataFrame(res, schema=schema)
+        res = df.groupBy("Repo").count().sort(["count"], ascending=[False])
+        print(res.take(5))
+    # top5_active_repo()
+
+    # Exercise 5:
+    def join_on_repo():
+        textfile = sc.textFile("data/interesting-repos.csv")
+        interestingRDD = textfile.map(lambda line: [line.split(",")[1]])
+        schema = StructType(
+            [StructField("url", StringType(), True)]
+        )
+        df_left = interestingRDD.toDF(schema=schema)
+
+        def pick(x):
+            return len(x) >= 4 and len(x[4].replace("//", "/").replace("?", "/").split('/')) > 4
+        def get_repo(x):
+            repo = x[4].replace("//", "/").replace("?", "/").split('/')
+            repo = "https://api.github.com/repos/" + repo[3] + '/' + repo[4]
+            return [repo]
+        df_right = rowrdd.filter(pick).map(get_repo).toDF(schema=schema)
+
+        res = df_left.join(df_right, ["url"])
+        print(res.count())
+        print(res.head())
+    # join_on_repo()
+
+    # Exercise 6:
+    def top5_failed_interesting_repo():
+        textfile = sc.textFile("data/interesting-repos.csv")
+        interestingRDD = textfile.map(lambda line: [line.split(",")[1]])
+        schema = StructType(
+            [StructField("url", StringType(), True)]
+        )
+        df_left = interestingRDD.toDF(schema=schema)
+
+        def pick(x):
+            if len(x) < 4 or x[4].find("Failed request") < 0:
+                return False
+            url = x[4].replace("URL: ", ",").split(',')[1]
+            repo = url.replace("//", "/").split('/')
+            return len(repo) > 4
+
+        def get_repo(x):
+            repo = x[4].replace("//", "/").replace("?", "/").split('/')
+            repo = "https://api.github.com/repos/" + repo[3] + '/' + repo[4]
+            return [repo]
+        df_right = rowrdd.filter(pick).map(get_repo).toDF(schema=schema)
+        df_right = df_right.groupBy("url").count()
+        res = df_left.join(df_right, ["url"])
+        res = res.sort(["count"], ascending=[False])
+        print(res.take(5))
+    # top5_failed_interesting_repo()
 
 
 if __name__ == "__main__":
